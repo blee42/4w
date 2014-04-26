13ba96ac96aeb8e80c0ab2de6078fd42cb354f6c
aspin <kevinchen@u.northwestern.edu>
2014-04-26 20:13:34 +0100

    updated route to explore

--- app.js
+++ app.js
@@ -153,7 +153,7 @@ app.get('/api/instagram', passportConf.isAuthenticated, passportConf.isAuthorize
 app.post('/api/instagram', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.postInstagram);
 
 // events routs
-app.post('/events', eventsController.getEvents);
+app.post('/explore', eventsController.getEvents);
 
 /**
  * OAuth routes for sign-in.
